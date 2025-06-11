de34d0836c36ee245aeaf8b06d38164d188f78ca
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-06-11 15:39:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ def grafico_autoavaliacao():
         empresa = auto.get("empresa", "Empresa")
         emailLider = auto.get("emailLider", "email")
         codrodada = auto.get("codrodada", "")
-        # numero_avaliacoes = len(auto.get("respostas", [])) // 96
+        
         data_hora = datetime.now().strftime("%d/%m/%Y %H:%M")
         
         plt.text(0.5, -0.18, subtitulo, ha="center", va="top", transform=ax.transAxes, fontsize=10)
