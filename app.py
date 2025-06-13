c2ed8b40f9d6da1d2042b27ccfb7af7e7585c992
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-06-13 14:34:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1481,7 +1481,7 @@ def termometro_microambiente():
             (8, "ESTÍMULO"),
             (13, "NEUTRO"),
             (17, "BAIXO ESTÍMULO"),
-            (50, "DESMOTIVAÇÃO➜")
+            (28, "DESMOTIVAÇÃO➜")
         ]
 
         for val, label in faixas:
@@ -1491,7 +1491,7 @@ def termometro_microambiente():
 
             # ⚠️ Se for o label "DESMOTIVAÇÃO ➜", ajusta a posição para a esquerda
             if "DESMOTIVAÇÃO" in label:
-                x -= 0.8  # desloca levemente à esquerda
+                x -= 0.4  # desloca levemente à esquerda
 
             ax.text(x, y, label, fontsize=9, ha='center', va='center', weight='bold')
 
