57f6c053b93cbe7011214cc24cf5929bdf0567e2
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-06-13 10:48:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1481,7 +1481,7 @@ def termometro_microambiente():
             (8, "ESTÍMULO"),
             (13, "NEUTRO"),
             (18, "BAIXO ESTÍMULO"),
-            (20, "DESMOTIVAÇÃO ➜")
+            (22, "DESMOTIVAÇÃO ➜")
 ]
 
         for val, label in faixas:
