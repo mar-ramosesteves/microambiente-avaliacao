bc08808e3221e7cd2b2699329a7e7d0ff9a84e2f
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-07-17 07:43:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
 from io import BytesIO
+from flask_cors import cross_origin
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["https://gestor.thehrkey.tech"]}}, supports_credentials=True)
