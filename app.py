743ce0bdd27c742116a83334c73012cd628252be
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-07-25 17:22:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -704,12 +704,7 @@ def salvar_grafico_media_equipe_dimensao():
             "subtitulo": f"{empresa} / {emaillider_req} / {codrodada} / {data_hora}", # Garanta que 'emaillider_req' está aqui
             "dados": resultado[["DIMENSAO", "IDEAL_%", "REAL_%"]].to_dict(orient="records") # Dados usando DIMENSAO
         }
-        # Salvar também o JSON com prefixo IA_ na subpasta ia_json
-        dados_json = {
-            "titulo": "MÉDIA DA EQUIPE - DIMENSÕES",
-            "subtitulo": f"{empresa} / {emaillider_req} / {codrodada} / {data_hora}",
-            "dados": resultado[["DIMENSAO", "IDEAL_%", "REAL_%"]].to_dict(orient="records")
-        }
+        
 
         # --- Chamar a função para salvar os dados do gráfico gerados no Supabase ---
         # Definir o tipo de relatório para o Supabase
